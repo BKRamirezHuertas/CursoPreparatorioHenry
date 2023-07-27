0c855755ab7a859d94f14c620264aedd534f5113
BKRamirezHuertas <ramirezhb3@gmail.com>
2023-07-27 00:46:29 -0500

    commit actualizado m3

--- controlDeFlujo.js
+++ controlDeFlujo.js
@@ -1,4 +1,4 @@
-function viajar(destino) {
+/* function viajar(destino) {
     if (destino === "Brasil") {
         console.log("Gire a la IZQUIERDA");
     } else if (destino === "Argentina") {
@@ -9,3 +9,14 @@ function viajar(destino) {
 }
 
 viajar("PRUEBA");
+ */
+
+function puedeManejar(edad) {
+    if (edad >= 18) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+puedeManejar('18');
